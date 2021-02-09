9470e02633136960ba542feccc33eaccf5836326
Matt Lant <matthewclant@gmail.com>
2021-02-09 01:28:53 -0700

    keep innovationmapper urls

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -191,7 +191,7 @@ class MainPage extends Component {
 
   render() {
 	if (!this.props.userIsAuthenticated) {
-		this.props.logout({ returnTo: 'http://localhost:3000' });
+		this.props.logout({ returnTo: 'https://innovationmapper.com' });
 	}
 	
 	let appClasses = 'App-header'
